95eeacb2f3ae77439542594f8d5ce0ef5f02357f
Nastya Ladynskaya <nastyaladynska@Nastyas-MacBook-Pro.local>
2024-06-30 19:58:14 +0300

    header layout

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <script setup>
+import Header from "./components/Header.vue"
 </script>
 
 <template>
+  <div class="bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-15">
+    <Header/>
+  </div>
 </template>
 
 <style scoped>
---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+    <header class="flex justify-between border-b border-slate-300 px-10 py-8">
+      <div class="flex items-center gap-4">
+        <img src="/logo.png" alt="logo" class="w-10">
+        <div>
+          <h2 class="text-xl font-bold uppercase">Vue Sneakers</h2>
+          <p class="text-slate-400">The biggest selection of sneakers</p>
+        </div>
+      </div>
+
+      <ul class="flex items-center gap-10">
+        <li class="flex items-center cursor-pointer gap-3 text-gray-600 hover:text-black">
+          <img src="/cart.svg" alt="cart">
+          <b>34$</b>
+        </li>
+      
+        <li class="flex items-center cursor-pointer gap-3 text-gray-600 hover:text-black">
+          <img src="/heart.svg" alt="heart">
+          <b>Favorites</b>
+        </li>
+      
+        <li class="flex items-center cursor-pointer gap-3 text-gray-600 hover:text-black">
+          <img src="/profile.svg" alt="profile">
+          <b>Profile</b>
+        </li>
+      </ul>
+
+    </header>
+</template>
