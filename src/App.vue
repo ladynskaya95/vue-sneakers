2d1e6c62c0bb8129c843ddf24f9e886aed16fb0f
Nastya Ladynskaya <nastyaladynska@Nastyas-MacBook-Pro.local>
2024-06-30 22:13:06 +0300

    card layout

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,24 @@
 <script setup>
 import Header from "./components/Header.vue"
+import Card from "./components/Card.vue"
 </script>
 
 <template>
-  <div class="bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-15">
+  <div class="bg-white w-4/5 m-auto rounded-xl shadow-xl mt-15">
     <Header/>
+
+   <div class="p-10">
+     <h2 class="text-3xl font-bold">All Sneakers</h2>
+
+     <div class="grid grid-cols-4 gap-5">
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+     </div> 
+   </div>
   </div>
 </template>
 
 <style scoped>
-
 </style>
