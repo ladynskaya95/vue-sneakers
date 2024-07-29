76d1a373a8b7b03764aaad9e7f9f4fc2bbc766ac
Nastya Ladynskaya <nastyaladynska@Nastyas-MacBook-Pro.local>
2024-07-29 23:34:17 +0300

    added input for search and sort

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,30 @@ import Drawer from "./components/Drawer.vue"
     <Header/>
 
    <div class="p-10">
-     <h2 class="text-3xl font-bold mb-8">All Sneakers</h2>
+     <div class="flex justify-between items-center">
+        <h2 class="text-3xl font-bold mb-8">All Sneakers</h2>
+
+    <div class="flex gap-4">
+        <select class="py-2 px-3 borded rounded-md outline-none">
+          <option>By name</option>
+          <option>By price (cheap)</option>
+          <option>By price (expensive)</option>
+        </select>
+
+        <div class="relative">
+          <img 
+            class="absolute left-4 top-3"
+            src="/search.svg" 
+            alt="search">
+          <input 
+            class="border rounded-md py-2 pl-11 pr-4 outline-none focus:border-gray-400"
+            placeholder="Search.."
+            type="text"/>
+        </div>
+    </div>
+    </div>
+
+
     <CardList/>
      
    </div>
